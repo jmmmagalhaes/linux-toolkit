9e85afa8845b1af655f47fccb39df894dafe3029
Alberon Ltd <info@alberon.co.uk>
2015-04-28 10:19:53 +0100

    Add script to forward all system accounts (mainly bounces) to a single address

---
+++ forward-system-mail.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o nounset -o pipefail -o errexit
+cd "$(dirname "$0")"
+
+# Get the server name
+server=$(hostname -s)
+
+# Loop through all cPanel users (don't use /home/* because it includes other directories too)
+for user in /var/cpanel/users/*; do
+    user=$(basename $user)
+    home=/home/$user
+    fwdfile=$home/.forward
+
+    # If the .forward file doesn't exist already, create it
+    if [ -d "$home" -a ! -f "$fwdfile" ]; then
+        # Display a message only if we create any - it will be sent as an email by cron
+        echo "Creating $fwdfile"
+        echo "servers+$user-$server@alberon.co.uk" > $fwdfile
+        chown $user:$user $fwdfile
+        chmod 644 $fwdfile
+    fi
+done

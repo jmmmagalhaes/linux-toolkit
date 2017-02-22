78cf1248111121f9f218402963b8cca3a0d17947
Alberon Ltd <info@alberon.co.uk>
2017-02-22 12:49:22 +0000

    Update PHP path to use CLI on all servers

--- cron-upgrade-composer-run.sh
+++ cron-upgrade-composer-run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if which php-cli >/dev/null 2>&1; then
-    # On CentOS the CGI version is the default not CLI
-    bin=php-cli
-else
-    bin=php
-fi
-
-exec $bin /usr/local/bin/composer self-update -q
+exec /usr/local/bin/php /usr/local/bin/composer self-update -q

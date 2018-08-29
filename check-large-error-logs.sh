a9409b42a5344afc86953a135c41f423e334453e
Nick Cook <nick@alberon.co.uk>
2018-08-29 11:56:14 +0100

    Remove size filtering on error_logs

--- check-large-error-logs.sh
+++ check-large-error-logs.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo 'List of error_log files larger than 1MB:'
-
 # List all error_log files on the server, sorted by largest first
-locate error_log | xargs du -h -t 1MB | sort -rh
+locate error_log | xargs du -h | sort -rh

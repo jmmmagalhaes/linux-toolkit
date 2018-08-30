3cc8a1b2282c817432c9b189701fd4a9c2d04b3e
Nick Cook <nick@alberon.co.uk>
2018-08-30 09:03:02 +0100

    Update locate to only return existing error_logs

--- check-large-error-logs.sh
+++ check-large-error-logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # List all error_log files on the server, sorted by largest first
-locate error_log | xargs du -h | sort -rh
+locate -e --regexp "\/error_log$" | xargs du -h | sort -rh

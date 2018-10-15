95487282c83c193d0f4efbabeba78e3005225873
Dave James Miller <dave@davejamesmiller.com>
2018-10-15 14:39:37 +0100

    Composer security check: Exclude suspended users; Look in other directories like vhosts/; Add verbose mode for debugging

--- check-composer-security.sh
+++ check-composer-security.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 set -o nounset -o pipefail -o errexit
 
-## loop through all the repo composer lock files
-for file in $(locate /home/**/repo/composer.lock)
-do
-    # check against sensiolabs
-    response=$(curl -H "Accept: text/plain" -s https://security.sensiolabs.org/check_lock -F lock=@$file);
+if [ "${1:-}" = "-v" ]; then
+    verbose=true
+else
+    verbose=false
+fi
 
-    if [[ ! $response =~ .*"No known* vulnerabilities detected"* ]]
-    then
-        echo "============================================================================================================="
-        echo $file
-        echo "============================================================================================================="
-        echo
-        echo "$response"
-        echo
+debug() {
+    if $verbose; then
+        echo "$1"
     fi
+}
+
+# Loop through all the repo composer lock files for active users
+debug "Looking for active accounts..."
+for account in $(cat /etc/passwd | grep -v :/bin/noshell$ | grep -v :/bin/false$ | grep -v :/sbin/nologin | cut -d: -f6 | grep ^/home/ | sort); do
+
+    debug "Locating composer.lock files in ${account}..."
+    for file in $(locate $account/**/composer.lock | grep -v /vendor/ | grep -v "^$account/.composer/"); do
+
+        # Check against sensiolabs
+        debug "Checking $file..."
+        response=$(curl -H 'Accept: text/plain' -s https://security.sensiolabs.org/check_lock -F lock=@$file)
+
+        if [[ ! $response =~ .*"No known* vulnerabilities detected"* ]]; then
+            echo "================================================================================"
+            echo " $file"
+            echo "================================================================================"
+            echo
+            echo "$response"
+            echo
+        fi
+    done
 done
+
+debug "Finished."

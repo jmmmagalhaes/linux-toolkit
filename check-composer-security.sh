b1c199ce120fc62b180758cfaf6bfc798a82b39d
Dave James Miller <dave@davejamesmiller.com>
2018-11-05 15:11:03 +0000

    New URL, new message, simplify command

--- check-composer-security.sh
+++ check-composer-security.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -o nounset -o pipefail -o errexit
+cd "$(dirname "$0")"
 
+# Parse options
 if [ "${1:-}" = "-v" ]; then
     verbose=true
 else
     verbose=false
 fi
 
+# Helpers
 debug() {
     if $verbose; then
         echo "$1"
@@ -15,16 +18,16 @@ debug() {
 
 # Loop through all the repo composer lock files for active users
 debug "Looking for active accounts..."
-for account in $(cat /etc/passwd | grep -v :/bin/noshell$ | grep -v :/bin/false$ | grep -v :/sbin/nologin | cut -d: -f6 | grep ^/home/ | sort); do
+for account in $(egrep -v ':(/bin/noshell|/bin/false|/sbin/nologin)$' /etc/passwd | cut -d: -f6 | grep ^/home/ | sort); do
 
     debug "Locating composer.lock files in ${account}..."
     for file in $(locate $account/**/composer.lock | grep -v /vendor/ | grep -v "^$account/.composer/"); do
 
         # Check against sensiolabs
         debug "Checking $file..."
-        response=$(curl -H 'Accept: text/plain' -s https://security.sensiolabs.org/check_lock -F lock=@$file)
+        response=$(curl -H 'Accept: text/plain' -s https://security.symfony.com/check_lock -F lock=@$file)
 
-        if [[ ! $response =~ .*"No known* vulnerabilities detected"* ]]; then
+        if [[ ! $response =~ "No packages have known vulnerabilities" ]]; then
             echo "================================================================================"
             echo " $file"
             echo "================================================================================"

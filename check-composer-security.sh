383ac4cfe3e45037ccf48cf0cc28f2c230640feb
Alberon Ltd <info@alberon.co.uk>
2018-12-18 11:48:19 +0000

    updated composer security check to use CLI tool (avoids hitting rate limits)

--- check-composer-security.sh
+++ check-composer-security.sh
@@ -9,6 +9,14 @@ else
     verbose=false
 fi
 
+if [ -t 0 ]; then
+    colors=
+else
+    colors=--no-ansi
+fi
+
+symfony="$HOME/.symfony/bin/symfony $colors"
+
 # Helpers
 debug() {
     if $verbose; then
@@ -16,26 +24,45 @@ debug() {
     fi
 }
 
+install() {
+    if [[ -f ~/.symfony/bin/symfony ]]; then
+        debug "Updating Symfony console"
+        $symfony self:update
+    else
+        debug "Installing Symfony console"
+        curl -sS https://get.symfony.com/cli/installer | bash
+    fi
+}
+
+check() {
+    debug "Locating composer.lock files in ${1}..."
+    for file in $(locate "$1/**/composer.lock" | grep -v /vendor/ | grep -v "^$1/.composer/"); do
+
+        # Check using symfony security checker
+        debug "Checking $file..."
+        directory=$(dirname "$file")
+
+        # ~/.symfony/bin/symfony security:check --dir=$directory
+        response=$($symfony security:check --dir="$directory" || true)
+        if [[ ! $response =~ "No packages have known vulnerabilities" ]]; then
+            debug ""
+           echo "================================================================================"
+           echo " $file"
+           echo "================================================================================"
+           echo
+           echo "$response"
+           echo
+        fi
+    done
+}
+
+# install / update Symfony console if needed
+install
+
 # Loop through all the repo composer lock files for active users
 debug "Looking for active accounts..."
 for account in $(egrep -v ':(/bin/noshell|/bin/false|/sbin/nologin)$' /etc/passwd | cut -d: -f6 | grep ^/home/ | sort); do
-
-    debug "Locating composer.lock files in ${account}..."
-    for file in $(locate $account/**/composer.lock | grep -v /vendor/ | grep -v "^$account/.composer/"); do
-
-        # Check against sensiolabs
-        debug "Checking $file..."
-        response=$(curl -H 'Accept: text/plain' -s https://security.symfony.com/check_lock -F lock=@$file)
-
-        if [[ ! $response =~ "No packages have known vulnerabilities" ]]; then
-            echo "================================================================================"
-            echo " $file"
-            echo "================================================================================"
-            echo
-            echo "$response"
-            echo
-        fi
-    done
+    check "$account"
 done
 
 debug "Finished."

66b8e65e8bcfcfe3be29c94223eedc2fcc1bd47a
Alberon Ltd <info@alberon.co.uk>
2019-01-14 16:03:34 +0000

    Fix Composer install script

--- composer-install.sh
+++ composer-install.sh
@@ -2,12 +2,4 @@
 set -o nounset -o pipefail -o errexit
 cd "$(dirname "$0")"
 
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-# Install cron job too?
-source bash/ask.sh
-
-echo
-if ask "Install cron job to upgrade Composer daily?" Y; then
-    ./cron-upgrade-composer-install.sh
-fi
+curl -sS https://getcomposer.org/installer | php -d allow_url_fopen=On -- --install-dir=/usr/local/bin --filename=composer

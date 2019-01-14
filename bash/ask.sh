e95f72cf9c0889810ada8c8face87e38ebcd304d
Alberon Ltd <info@alberon.co.uk>
2019-01-14 16:03:51 +0000

    Reuse ask() function

--- bash/ask.sh
+++ bash/ask.sh
@@ -1,32 +1,33 @@
-# This is a general-purpose function to ask Yes/No questions in Bash, either
-# with or without a default answer. It keeps repeating the question until it
-# gets a valid answer.
-
 ask() {
-    # http://djm.me/ask
+    # https://gist.github.com/davejamesmiller/1965569
+    local prompt default reply
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
     while true; do
 
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
 
-        # Ask the question
-        read -p "$1 [$prompt] " REPLY
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
 
         # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
+        if [ -z "$reply" ]; then
+            reply=$default
         fi
 
         # Check if the reply is valid
-        case "$REPLY" in
+        case "$reply" in
             Y*|y*) return 0 ;;
             N*|n*) return 1 ;;
         esac
